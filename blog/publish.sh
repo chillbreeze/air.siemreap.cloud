24d907b4059a6ab68fc5b931cb25ce373e04b203
chillbreeze <tcolvin79@gmail.com>
2026-04-09 13:14:41 +0700

    Remove blog from website

--- blog/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run this from the blog/ directory whenever you add or edit a post
-set -e
-echo "Building blog..."
-pelican content -s pelicanconf.py
-echo "Done. Files are in ./output/"
